73fb41d975d0c33a994ab0bff79f594762ef0f71
Mingxin.Shen <mingxin.shen20@imperial.ac.uk>
2021-07-26 15:17:55 +0800

    fix some errors, add randomDisatser function

--- js/game.js
+++ js/game.js
@@ -4,21 +4,24 @@
 define all the constants here
 */
 const TECH_POP_COEFT = 0.2 // techpoints generated per capital
-const SEA_MAX = 1500 // terminational condition concerning sea pollution
-const POP_MIN = 1
-const ECO_MAX = 2000
-const RD_MAX, RD_MIN, TECH_MAX = 10 // tmp defined
-const FERT_BASE = 0.5
+const SEA_MAX = 1500 // termination condition concerning sea pollution
+const POP_MIN = 1;
+const ECO_MAX = 2000;
+const RD_MAX, RD_MIN; // TODO define termination condition in terms of time scale
+const TECH_MAX = 10 // tmp defined
+const FERT_BASE = 0.5 // TODO suggest to set to 0, this determines the average maximum popultion under the current citycapacity, eg. o.5 fert-base corresponds to 1.5 * city capacity
 const MORT_BASE = 0.5 // may remove later
-const EQ_COEFT = 5 // tmp defined
+const EQ_COEFT = 0.05 // tmp defined
 const DOCK_POLL = 100
 const CITY_POLL = 200
 const CITY_CONS = 1000 // city consumption, 一回合消耗1000 resource points, 能linear about population 更好
 const FOREST_ECOB = 50 // forest recovery in terms of ecoImbalance
 const POWERP_ECOI = 200 // powerplant harm to ecobalance
-    /*
-    define all the variables here
-    */
+const SEA_REC = 200 // sea pollution self recovery
+
+/*
+define all the variables here
+*/
 
 var population, resourcePoints, techPoints, cityCapacity
 var earthquakeLikelihood, seaPollution, ecoImbalance, ecoImbalance;
@@ -50,11 +53,11 @@ function updateValues(arrayPerRound) {
     var resourcePoints_old = arrayPerRound[1];
     var ecoImbalance_old = arrayPerRound[4];
     var population_old = arrayPerRound[0];
-    var earthquakeLikelihood = (powerPlant + farmland - forest + city) * EQ_COEFT
+    var earthquakeLikelihood = (numPowerPlant + numFarm - numForest + numCity) * EQ_COEFT
     var population_new = (FERT_BASE + numTech / TECH_MAX * 0.5 - seaPollution / SEA_MAX + (cityCapcity - population_old) / cityCapacity) *
-        population_old + population_old - randomDisaster(earthquakeLikelihood)
+        population_old + population_old - randomDisaster(earthquakeLikelihood, population_old)
         // var population_new = (coefficient * (1 - population_old / cityCapacity) - seaPollution / SEA_MAX) * population_old + population_old - randomDisaster(earthquakeLikelihood)
-    var seaPollution_new = (seaPollution_old + DOCK_POLL * numDock + CITY_POLL * numCity) * 1.01 - 200 - conservation
+    var seaPollution_new = (seaPollution_old + DOCK_POLL * numDock + CITY_POLL * numCity) * 1.01 - SEA_REC - conservation
         // dock 是码头 city是城镇 每回合码头和城镇都会对海洋产生污染 每回合海洋自己可以修复200
 
     var ecoImbalance_new = ecoImbalance_old + CITY_POLL * numCity + POWERP_ECOI * numPowerPlant - FOREST_ECOB * numForest - conservation
@@ -64,6 +67,20 @@ function updateValues(arrayPerRound) {
     var arrayPerRound = [population_new, resourcePoints_new, earthquakeLikelihood, seaPollution_new, ecoImbalance_new, techPoints_new]
 }
 
+// not really verified method of generating normally distributed values in js, credit given to https://mika-s.github.io/javascript/random/normal-distributed/2019/05/15/generating-normally-distributed-random-numbers-in-javascript.html
+
+function randomDisaster(p, n) {
+    mean = n * p;
+    variance = n * p * (1 - p); // here uses a binomial distribution variance, but variance correlates with distribution of population on the map might be more favoruable, this requires access to map
+    stdev = Math.sqrt(variance);
+
+    // boxMullerTransform
+    const u1 = Math.random();
+    const u2 = Math.random();
+    const z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(2.0 * Math.PI * u2);
+    return z0 * stddev + mean;
+
+}
 
 // 6 types of items: city, farm, dock, powerplant, forest
 // functions of how adding/removing an item affect resource/tech point and other variables

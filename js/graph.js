7c0acd5da7c4928fa4bcd113fd4deaef7bb763a8
Tian Xiao <tianxiaos1202@gmail.com>
2021-07-28 11:10:50 +0800

    Update game.html

--- js/graph.js
+++ js/graph.js
@@ -1,8 +1,25 @@
 function showList() {
     var lst = document.getElementById("list-popout-box");
+    var tech = document.getElementById("tech-popout-box");
+
+    tech.style.display = "none";
+
     if (lst.style.display === "none") {
         lst.style.display = "block";
     } else {
         lst.style.display = "none";
+    }
+}
+
+function showTech() {
+    var lst = document.getElementById("list-popout-box");
+    var tech = document.getElementById("tech-popout-box");
+
+    lst.style.display = "none";
+
+    if (tech.style.display === "none") {
+        tech.style.display = "block";
+    } else {
+        tech.style.display = "none";
     }
 }

25ed57f1e387b0a9b48c71ff3d58868637a6a635
Mingxin.Shen <mingxin.shen20@imperial.ac.uk>
2021-07-30 18:00:37 +0800

    fix animation.js

--- js/animation.js
+++ js/animation.js
@@ -4,3 +4,13 @@ window.transitionToPage = function(href) {
         window.location.href = href
     }, 500)
 }
+
+if (document.readyState !== 'loading') {
+    console.log('document is already ready, just execute code here');
+    document.querySelector('body').style.opacity = 1
+} else {
+    document.addEventListener('DOMContentLoaded', function() {
+        console.log('document was not ready, place code here');
+        document.querySelector('body').style.opacity = 1
+    });
+}
--- js/game.js
+++ js/game.js
@@ -5,6 +5,16 @@ window.transitionToPage = function(href) {
     }, 500)
 }
 
+if (document.readyState !== 'loading') {
+    console.log('document is already ready, just execute code here');
+    document.querySelector('body').style.opacity = 1
+} else {
+    document.addEventListener('DOMContentLoaded', function() {
+        console.log('document was not ready, place code here');
+        document.querySelector('body').style.opacity = 1
+    });
+}
+
 var onHand = null;
 
 function setShovel() {
